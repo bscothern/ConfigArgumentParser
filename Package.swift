31c666460dff887355d5d36fc5a4d8ead0e92279
Braden Scothern <BradenScothern@gmail.com>
2021-09-29 18:37:40 -0600

    Updated swift-argument-parser to v1.0.1+

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.1"),
     ],
     targets: [
         .target(
