904c88d1b9f1402126257f4334e3aa3e2f68ee5e
Braden Scothern <BradenScothern@gmail.com>
2021-10-06 18:25:30 -0600

    More windows CI debugging...

--- Tests/ConfigArgumentParserTests/ConfigArgumentParserExamplesTests.swift
+++ Tests/ConfigArgumentParserTests/ConfigArgumentParserExamplesTests.swift
@@ -78,6 +78,7 @@ final class ConfigArgumentParserExamplesTests: XCTestCase {
         }
         print(#file)
         print(packageRoot.string)
+        print((try? FileManager.default.contentsOfDirectory(atPath: packageRoot.string)) ?? [])
         return packageRoot
     }()
 
